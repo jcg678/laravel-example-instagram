33775ed4eea268047829170543861eecb4355b55
jcg678 <jcg678@hotmail.es>
2019-05-24 18:46:14 +0200

    ejemplos para bbdd

--- database.sql
+++ database.sql
@@ -18,6 +18,10 @@ CONSTRAINT pk_users PRIMARY KEY(id)
 
 )ENGINE=InnoDb;
 
+INSERT INTO users VALUES(NULL,'user','Javier', 'Criado', 'jcg678', 'demo@demo.com','pass', null, CURTIME(), CURTIME(), NULL);
+INSERT INTO users VALUES(NULL,'user','Juan', 'Garcia', 'juan678', 'demo2@demo.com','pass', null, CURTIME(), CURTIME(), NULL);
+INSERT INTO users VALUES(NULL,'user','Paco', 'MArtinez', 'paco678', 'demo3@demo.com','pass', null, CURTIME(), CURTIME(), NULL);
+
 CREATE TABLE IF NOT EXISTS images(
 id int(255) auto_increment not null,
 user_id int(255),
@@ -29,6 +33,12 @@ CONSTRAINT pk_images PRIMARY KEY(id),
 CONSTRAINT fk_images FOREIGN KEY(user_id) REFERENCES users(id)
 
 )ENGINE=InnoDb;
+
+INSERT INTO images VALUES(NULL, 1, 'test.jpg', 'description de prueba 1', CURTIME(),CURTIME() );
+INSERT INTO images VALUES(NULL, 1, 'playa.jpg', 'description de prueba playa', CURTIME(),CURTIME() );
+INSERT INTO images VALUES(NULL, 1, 'arena.jpg', 'description de prueba arena', CURTIME(),CURTIME() );
+INSERT INTO images VALUES(NULL, 3, 'familia.jpg', 'description de prueba arena', CURTIME(),CURTIME() );
+
 
 CREATE TABLE IF NOT EXISTS comments(
 id int(255) auto_increment not null,
@@ -42,6 +52,10 @@ CONSTRAINT fk_comments_users FOREIGN KEY(user_id) REFERENCES users(id),
 CONSTRAINT fk_comments_images FOREIGN KEY(image_id) REFERENCES images(id)
 )ENGINE=InnoDb;
 
+INSERT INTO comments values(NULL, 1, 4, 'Buena foto de familia', CURTIME(), CURTIME());
+INSERT INTO comments values(NULL, 2, 1, 'Buena foto de playa', CURTIME(), CURTIME());
+INSERT INTO comments values(NULL, 2, 4, 'Ouhh yeahh', CURTIME(), CURTIME());
+
 CREATE TABLE IF NOT EXISTS likes(
 id int(255) auto_increment not null,
 user_id int(255),
@@ -52,3 +66,9 @@ CONSTRAINT pk_likes PRIMARY KEY(id),
 CONSTRAINT fk_likes_users FOREIGN KEY(user_id) REFERENCES users(id),
 CONSTRAINT fk_likes_images FOREIGN KEY(image_id) REFERENCES images(id)
 )ENGINE=InnoDb;
+
+INSERT INTO likes VALUES(NULL,1,4,CURTIME(),CURTIME());
+INSERT INTO likes VALUES(NULL,2,4,CURTIME(),CURTIME());
+INSERT INTO likes VALUES(NULL,3,1,CURTIME(),CURTIME());
+INSERT INTO likes VALUES(NULL,3,2,CURTIME(),CURTIME());
+INSERT INTO likes VALUES(NULL,2,1,CURTIME(),CURTIME());
