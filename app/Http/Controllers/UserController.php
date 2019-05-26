25381a141c659d3198fb62dced349d6aeba73909
jcg678 <jcg678@hotmail.es>
2019-05-26 21:00:16 +0200

    validacion config

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,15 +12,21 @@ class UserController extends Controller
 
     public function update(Request $request){
     	$id = \Auth::user()->id;
+    	$validate =$this->validate($request,[
+    		'name' => ['required', 'string', 'max:255'],
+            'surname' => ['required', 'string', 'max:255'],
+            'nick'=>'required|string|max:255|unique:users,nick,'.$id,
+            //'nick' => ['required', 'string', 'max:255'],
+            'email'=>'required|string|max:255|unique:users,email,'.$id,            
+            //'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+    	]);	
+
+
     	$name=$request->input('name');
     	$surname=$request->input('surname');
     	$nick=$request->input('nick');
     	$email=$request->input('email');
 
-    	var_dump($id);
-    	var_dump($name);
-    	var_dump($surname);
-    	var_dump($email);
-    	die();
+
     }	
 }
