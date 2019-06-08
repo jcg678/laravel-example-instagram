1709ce4a0894b4860d6c54981f734d06fa3d481e
jcg678 <jcg678@hotmail.es>
2019-06-08 12:25:31 +0200

    maquetando likes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,10 +23,19 @@
                         <div class="image-container">
                             <img src="{{ route('image.file',['filename'=>$image->image_path]) }}" />
                         </div>
-                        <div class="likes"></div>
+
                         <div class="description">
                             <span class="nickname"> {{'@'.$image->user->nick}}</span>
                             <p> {{$image->description}}<p>
+                        </div>
+                         <div class="likes">
+                            <img src="{{asset('img/heart-black.png')}}">
+
+                        </div>
+                        <div class="comments">
+                            <a href="" class="btn btn-sm btn-warning btn-comments">
+                                Comentarios    
+                            </a>
                         </div>
                     </div>
                 </div>
