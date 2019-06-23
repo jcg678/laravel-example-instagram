01d29a436142909e20433a9c5060345a8451fa73
jcg678 <jcg678@hotmail.es>
2019-06-23 18:26:56 +0200

    cambiando boton de color

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,26 @@
 window.addEventListener("load",function(){
-	alert("la pagina esta cargada");
+	$('.btn-like').css('cursor','pointer');
+	$('.btn-dislike').css('cursor','pointer');
+
+
+	function dislike(){
+		$('.btn-dislike').unbind('click').click(function(){
+			$(this).addClass('btn-like').removeClass('btn-dislike');
+			$(this).attr('src', 'img/heart-black.png');
+			like();
+		});
+		
+	}
+	dislike();
+
+	function like(){
+		$('.btn-like').unbind('click').click(function(){
+			$(this).addClass('btn-dislike').removeClass('btn-like');
+			$(this).attr('src', 'img/heart-red.png');
+			dislike();
+		});
+
+	}
+	like();
 });
+
