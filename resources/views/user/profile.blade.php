9b1f894e8c84df8df1c70efa0fe3c2ae9e927d69
jcg678 <jcg678@hotmail.es>
2019-06-28 19:23:44 +0200

    datos de perfil

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -4,6 +4,20 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+        	<div class="data-user">
+        		
+                        @if($user->image)
+                         <div class="container-avatar"> 
+                            <img src="{{route('user.avatar',['filename'=>$user->image]) }}" class="avatar" />
+                        </div>
+                        @endif
+                  
+                 <div class="user-info">
+                 	<h1>{{'@'.$user->nick}}</h1>
+                 	<h1>{{$user->name}}</h1>
+                 	<p>{{ 'Se unió hace: '.\FormatTime::LongTimeFilter($user->created_at)}}</p>
+                 </div>     
+        	</div> 
             @include('includes.message')
             @foreach($user->images as $image)
                 @include('includes.image',['image'=>$image])
