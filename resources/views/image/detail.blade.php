4d562baeb80f2e95ef616b17e377a73f72252562
jcg678 <jcg678@hotmail.es>
2019-07-01 18:06:27 +0200

    modal para borrar

--- resources/views/image/detail.blade.php
+++ resources/views/image/detail.blade.php
@@ -47,8 +47,41 @@
                         </div>
                         @if(Auth::user() && Auth::user()->id == $image->user->id)
                         <div class="actions">
-                             <a href="" class="btn btn-sm btn-primary">Actulizar</a>
-                             <a href="{{ route('image.delete',['id'=>$image->id]) }}" class="btn btn-sm btn-danger">Borrar</a>
+                             <a href="" class="btn btn-sm btn-primary">Actualizar</a>
+                             
+
+                            <!-- Button to Open the Modal -->
+                            <button type="button" class="btn btn-sm btn-danger" data-toggle="modal" data-target="#myModal">
+                              Borrar
+                            </button>
+
+                            <!-- The Modal -->
+                            <div class="modal" id="myModal">
+                              <div class="modal-dialog">
+                                <div class="modal-content">
+
+                                  <!-- Modal Header -->
+                                  <div class="modal-header">
+                                    <h4 class="modal-title">¿Estas seguro?</h4>
+                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                  </div>
+
+                                  <!-- Modal body -->
+                                  <div class="modal-body">
+                                    Si eliminas esta imagen nunca podrás recuperarla ¿Estas seguro?
+                                  </div>
+
+                                  <!-- Modal footer -->
+                                  <div class="modal-footer">
+                                    <button type="button" class="btn btn-success" data-dismiss="modal">Cancelar</button>
+                                    <a href="{{ route('image.delete',['id'=>$image->id]) }}" class="btn  btn-danger">Borrar Definitivamente</a>
+                                  </div>
+
+                                </div>
+                              </div>
+                            </div>
+
+
                         </div>    
                         @endif
                         <div class="clear-fix"></div>
